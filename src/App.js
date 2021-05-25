45d59f25e4bcedb95538dd115a6540f403f5b4cc
aoihanabi <abii98cm@gmail.com>
2021-05-24 18:07:11 -0600

    Learned about function and class react components, also added an onclick event with a normal js function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const App = () => {
-    return <h1>Hi React World</h1>;
+function sayHello () {
+    console.log("Hello from the cute button")
 }
 
+//React function component example
+const Header = function(){
+    return <h1>Click the button!</h1>
+}
+
+class Button extends React.Component {
+    render() {
+        return <button onClick={sayHello}>Click me...</button>
+    }
+}
+
+//React Class component example
+class App extends React.Component //the inheritance is what makes it a react class by making available react methods to it.
+{
+    //this is a React method.
+    render() {
+        return (
+        <>  
+            <Header />
+            <Button />
+        </>);
+    }
+}
 export default App;
